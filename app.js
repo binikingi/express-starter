2b576161ccdc6827dfa3da90e3deba3e3f562784
Bini <bgt636>
2018-09-16 18:16:26 +0300

    picit branch

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/checkbranch', (req,res) => {
+  res.status(200).send('branch ok');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
